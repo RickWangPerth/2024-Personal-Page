a1f1686de7b6cc17eaba6eddfea1b7808824033a
Xinlyu Wang <xinlalalele@gmail.com>
2024-02-18 00:21:28 +0800

    Add newsletter feature

--- PH2024/src/app/page.tsx
+++ PH2024/src/app/page.tsx
@@ -22,29 +22,30 @@ import image4 from '@/images/photos/image-4.jpg'
 import image5 from '@/images/photos/image-5.jpg'
 import { type ArticleWithSlug, getAllArticles } from '@/lib/articles'
 import { formatDate } from '@/lib/formatDate'
+import Newsletter from '@/components/Newsletter'
 
-function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
+// function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+//   return (
+//     <svg
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       strokeWidth="1.5"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       aria-hidden="true"
+//       {...props}
+//     >
+//       <path
+//         d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
+//         className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+//       />
+//       <path
+//         d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
+//         className="stroke-zinc-400 dark:stroke-zinc-500"
+//       />
+//     </svg>
+//   )
+// }
 
 function BriefcaseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
@@ -110,49 +111,50 @@ function SocialLink({
   )
 }
 
-function Newsletter() {
-  return (
-    <form
-      action="https://gmail.us22.list-manage.com/subscribe/post?u=228793f33bf0242dabfc8d578&amp;id=2ccec90a82&amp;f_id=00fcc2e1f0"
-      method="post"
-      id="mc-embedded-subscribe-form"
-      name="mc-embedded-subscribe-form"
-      className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
-      target="_self"
-      noValidate
-    >
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        {/* MailIcon should be an actual SVG or component */}
-        <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
-      </h2>
-      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        Get notified when I publish something new, and unsubscribe at any time.
-      </p>
-      <div className="mt-6 flex">
-        <input
-          type="email"
-          name="EMAIL"
-          placeholder="Email address"
-          aria-label="Email address"
-          required
-          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
-        />
-        {/* The Button component should render an input of type submit */}
-        <Button type="submit" className="ml-4 flex-none">
-          Join
-        </Button>
-      </div>
-      {/* Include any hidden inputs required by MailChimp */}
-      <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
-        <input type="text" name="b_228793f33bf0242dabfc8d578_2ccec90a82" tabIndex={-1} />
-      </div>
-    </form>
-  )
-}
-
-// Remember to replace MailIcon with the actual icon component and Button with your button component
-
+// function Newsletter() {
+//   return (
+//     <div id="mc_embed_shell">
+//       <div id="mc_embed_signup">
+//         <form
+//           action="https://gmail.us8.list-manage.com/subscribe/post?u=c0e4f733a3ee7ac29a92350a7&amp;id=60015f70c5&amp;f_id=00a617e0f0"
+//           method="post"
+//           id="mc-embedded-subscribe-form"
+//           name="mc-embedded-subscribe-form"
+//           className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40 validate"
+//           target="_self"
+//           noValidate
+//         >
+//           <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+//             {/* MailIcon should be an actual SVG or component */}
+//             <MailIcon className="h-6 w-6 flex-none" />
+//             <span className="ml-3">Stay up to date</span>
+//           </h2>
+//           <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+//             Get notified when I publish something new, and unsubscribe at any time.
+//           </p>
+//           <div className="mt-6 flex">
+//             <input
+//               type="email"
+//               name="EMAIL"
+//               placeholder="Email address"
+//               aria-label="Email address"
+//               required
+//               className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
+//             />
+//             {/* The Button component should render an input of type submit */}
+//             <Button type="submit" className="ml-4 flex-none">
+//               Join
+//             </Button>
+//           </div>
+//           {/* Include any hidden inputs required by MailChimp */}
+//           <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+//             <input type="text" name="b_c0e4f733a3ee7ac29a92350a7_60015f70c5" tabIndex={-1} />
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   )
+// }
 
 interface Role {
   company: string
@@ -302,7 +304,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Application designer, data magician, and psychology enthusiast.
+            Application designer, data magician, and sychology enthusiast.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Rick, a application designer and AI & IT solution provider based in Perth. 
---
+++ PH2024/src/components/Newsletter.tsx
@@ -0,0 +1,137 @@
+'use client'
+import React, { useRef, FormEvent, useEffect, useState } from 'react'
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
+import { useRouter } from 'next/navigation'
+import { Button } from '@/components/Button'
+
+function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <path
+        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
+        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+      />
+      <path
+        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
+        className="stroke-zinc-400 dark:stroke-zinc-500"
+      />
+    </svg>
+  )
+}
+
+const url = `${process.env.NEXT_PUBLIC_MAILCHIMP_URL}`
+
+interface CustomFormProps {
+  onSubmitted: (formData: { EMAIL: string }) => void
+  subscribeStatus: 'idle' | 'sending' | 'error' | 'success' | null
+}
+
+const CustomForm: React.FC<CustomFormProps> = ({
+  onSubmitted,
+  subscribeStatus,
+}) => {
+  const emailInput = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (emailInput.current && emailInput.current.value.includes('@')) {
+      onSubmitted({
+        EMAIL: emailInput.current.value,
+      })
+    }
+  }
+
+  let buttonText = 'Join'
+  if (subscribeStatus === 'sending') {
+    buttonText = 'Sending...'
+  }
+  if (subscribeStatus === 'success') {
+    buttonText = 'Thank you!'
+  } else if (subscribeStatus === 'error') {
+    buttonText = 'Error :('
+  }
+
+  return (
+    <form
+      className="validate rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
+      onSubmit={handleSubmit}
+      noValidate
+    >
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <MailIcon className="h-6 w-6 flex-none" />
+        <span className="ml-3">Stay up to date</span>
+      </h2>
+      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+        Get notified when I publish something new, and unsubscribe at any time.
+      </p>
+      <input
+        ref={emailInput}
+        type="email"
+        name="EMAIL"
+        placeholder="Email address"
+        aria-label="Email address"
+        required
+        className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
+      />
+      <Button type="submit" className="ml-4 flex-none">
+        {buttonText}
+      </Button>
+    </form>
+  )
+}
+
+const Newsletter: React.FC = () => {
+  const router = useRouter()
+  const [subscribeStatus, setSubscribeStatus] = useState<
+    'idle' | 'sending' | 'error' | 'success'
+  >('idle')
+
+  useEffect(() => {
+    if (subscribeStatus === 'success') {
+      const timer = setTimeout(() => {
+        router.push('/thank-you')
+      }, 1500)
+      return () => clearTimeout(timer)
+    }
+  }, [subscribeStatus, router])
+
+  return (
+    <div id="mc_embed_shell">
+      <div id="mc_embed_signup">
+        <MailchimpSubscribe
+          url={url}
+          render={({ subscribe, status, message }) => {
+            // 如果状态变化，更新本地状态以触发 useEffect
+            if (status !== null && status !== subscribeStatus) {
+              setSubscribeStatus(status)
+            }
+
+            return (
+              <>
+                <CustomForm
+                  onSubmitted={(formData) => subscribe(formData)}
+                  subscribeStatus={subscribeStatus}
+                />
+                {/* {status === 'sending' && <div>sending...</div>}
+                {status === 'error' && (
+                  <div dangerouslySetInnerHTML={{ __html: message || '' }} />
+                )}
+                {status === 'success' && <div>Success!</div>} */}
+              </>
+            )
+          }}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Newsletter
