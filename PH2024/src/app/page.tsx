2cdbb3341b3285af892e8efc2da9c5dae90d48b7
Xinlyu Wang <xinlalalele@gmail.com>
2024-03-04 10:49:58 +0800

    Fix typo

--- PH2024/src/app/page.tsx
+++ PH2024/src/app/page.tsx
@@ -310,7 +310,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Application designer, data artist, and sychology enthusiast.
+            Application designer, Data artist, and Psychology enthusiast.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Rick, a application designer and AI & IT solution provider based
